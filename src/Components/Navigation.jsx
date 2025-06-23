1d8225a2768fc8cf23c8fa14a4dd961060e37ba6
ahmadh-najahi <ahmednajahi2003@gmail.com>
2025-06-24 01:06:16 +0530

    feat: add Navigation component with styled container

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+function Navigation() {
+    return (
+        <NavigationStyle>
+            
+        </NavigationStyle>
+    )
+}
+
+const NavigationStyle = styled.div`
+  
+`;
+
+export default Navigation
