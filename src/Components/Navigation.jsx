c4533a941b5fa2da821e86e5be8b7ffaf943385d
ahmadh-najahi <ahmednajahi2003@gmail.com>
2025-06-25 13:39:07 +0530

    feat: update Navigation component to accept dynamic list prop and integrate into TopBar

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,16 +1,24 @@
+import { Link, Links } from "react-router-dom";
 import styled from "styled-components";
+import React from "react";
 
 
-function Navigation() {
+function Navigation({list = [], className , ...props}) {
     return (
-        <NavigationStyle>
-            
+        <NavigationStyle {...props} className={`${className}`}>
+            <section className="d-flex">
+                {
+                    list.map(({ path, text }, i) => (
+                        <Link to={path} key={i}>{text}</Link>
+                    ))
+                }
+            </section>
         </NavigationStyle>
     )
 }
 
 const NavigationStyle = styled.div`
   
-`;
+`;  
 
 export default Navigation
--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,11 +1,30 @@
 import styled from "styled-components";
 import Logo from "./Logo";
+import Navigation from "./Navigation";
 
 
 function TopBar() {
     return (
         <TopBarStyle>
             <Logo text="Dash Box" />
+            <Navigation list={[
+                {
+                    path: "/",
+                    text: "Home"
+                },
+                {
+                    path: "/work",
+                    text: "How It Work"
+                },
+                {
+                    path: "/faq",
+                    text: "FAQ"
+                },
+                {
+                    path: "/pricing",
+                    text: "Pricing"
+                },
+            ]} />
         </TopBarStyle>
     )
 }
