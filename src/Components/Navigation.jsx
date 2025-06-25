8a1e12da52242a5e84a3c455eac51b4b742e675a
ahmadh-najahi <ahmednajahi2003@gmail.com>
2025-06-25 22:50:32 +0530

    feat(Navigation): add mobile menu with responsive styling
    Implement responsive navigation for mobile devices by adding a hamburger menu button and dropdown menu. The menu appears only on small screens (hidden on medium+) and includes styled links matching the desktop navigation.

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,6 +2,7 @@ import { Link, Links } from "react-router-dom";
 import styled from "styled-components";
 import React from "react";
 import { media } from "../Services";
+import { BiMenu } from "react-icons/bi";
 
 
 function Navigation({ list = [], className, ...props }) {
@@ -17,9 +18,17 @@ function Navigation({ list = [], className, ...props }) {
 
 
 
-            <button>
-                
+            <button className="d-block d-md-none nav-sm-button" >
+                <BiMenu />
             </button>
+
+            <nav className="d-block d-md-none small-device-nav-container">
+                {
+                    list.map(({ path, text }, i) => (
+                        <Link className="navigation-sm-link" to={path} key={i}>{text}</Link>
+                    ))
+                }
+            </nav>
         </NavigationStyle>
     )
 }
@@ -28,7 +37,7 @@ const NavigationStyle = styled.div`
   position: relative;
   
 
-  .navigation-link{
+  .navigation-link,.navigation-sm-link{
     text-decoration: none;
     font-family: "DM Sans", sans-serif;
     font-weight: 700;
@@ -49,6 +58,22 @@ const NavigationStyle = styled.div`
     &:hover{
         opacity:  0.7;
     }
+  }
+
+  .nav-sm-button{
+    background-color: transparent;
+    border: none;
+    color: white ;
+    font-size: 35px ;
+    cursor: pointer;
+  }
+
+  .small-device-nav-container{
+    position: absolute ;
+    top: 50px;
+    display: flex ;
+    flex-direction: column;
+    width: 200px ;
   }
 `;
 
